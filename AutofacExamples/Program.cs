44af6f0b0995c4712665f62a6a241585a21d2d8d
bentayloruk <ben.taylor.uk@gmail.com>
2011-06-13 14:06:02 +0100

    Code for SO question 6327762

---
+++ AutofacExamples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AutofacExamples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new SoAnswer6327762().DoExample();
+        }
+    }
+}
